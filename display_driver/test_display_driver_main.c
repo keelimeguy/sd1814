f6ebf53ea90727ffe154643ee36819f93d372a16
Keelin Becker-Wheeler <keelinwheeler@gmail.com>
2017-12-04 02:27:42 -0500

    Fixing text bugs in display_driver

--- display_driver/test_display_driver_main.c
+++ display_driver/test_display_driver_main.c
@@ -2,19 +2,22 @@
 */
 
 #include "display_driver.h"
+#include "GFXfont/Fonts/FreeMono9pt7b.h"
 
 int main() {
     disp_init();
+    disp_set_font(&FreeMono9pt7b);
 
     disp_start_write();
-    disp_draw_line(0,0,63,63,1);
+    disp_set_pos(0,0);
+    disp_write_str("\\Hello, world!");
     disp_end_write();
-
-    disp_setPos(0,20);
-
-    disp_start_write();
-    disp_write_str("A");
-    disp_end_write();
+    uint8_t x1;
+    uint8_t y1;
+    uint8_t w;
+    uint8_t h;
+    disp_get_text_bounds("\\Hello, world!", 0, 0, &x1, &y1, &w, &h);
+    printf("%d, %d, %d, %d\n", x1, y1, w, h);
 
     return 0;
 }
